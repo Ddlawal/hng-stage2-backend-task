1683dc93e35d5c2795e79186fb74c43ce220c977
Muhammad Lawal Adamu <lawaldd@gmail.com>
2021-08-20 13:02:47 +0100

    Few changes

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ config({ path: path.join(__dirname, '../.env') })
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(urlencoded({ extended: false }))
